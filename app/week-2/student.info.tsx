d95ad99de07d434bbdb36a5636e05fc4cafc5eaf
RiteshPatel17 <patelriteshkumarh@gmail.com>
2026-01-22 14:53:51 -0700

    final

--- app/week-2/student.info.tsx
+++ app/week-2/student.info.tsx
@@ -4,12 +4,12 @@ export default function StudentInfo() {
   return (
     <section>
       <p>Your Name: Ritesh Patel</p>
-      <p>
-        GitHub Repository:{" "}
+
+        
         <Link href="https://github.com/RiteshPatel17/cprg306-assignments">
           My GitHub Repository
         </Link>
-      </p>
+    
     </section>
   );
 }
